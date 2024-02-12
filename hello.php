9d06ccb9cd94cd60a97cbc3615da542b708d54ba
unknown <mariemboudabous0512@gmail.com>
2024-02-12 16:55:25 +0100

    test

---
+++ hello.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+  <ul>     
+<?php
+/*echo "<b>mon premier test php!<b><br>";
+$a=1;
+echo "la valeur de a est $a<br>";
+$a="str";
+echo $a;*/
+$tab1[0] = "Arij" ;
+$tab1[1] = "Mariem" ;
+$tab1[2] = "Yassmine" ;
+$tab1[3] = "Nour" ;
+foreach($tab1 as $v)
+{
+echo "<li>$v</li>\n";
+}
+?>
+</ul>
+</html>
